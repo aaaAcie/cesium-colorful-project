cb8edd13faac1fabc53153dbb15c8ef7b41c26ba
徐亦快 <913587892@qq.com>
2022-04-18 17:55:45 +0800

    完成图表+1

---
+++ src/views/neighborhood/echarts/secondLeft.vue
@@ -0,0 +1,128 @@
+<template>
+  <common-echart :pos="pos" :option="option" @getMychart="getMychart" width="320" height="125"></common-echart>
+</template>
+
+<script setup>
+  import { reactive, computed } from 'vue'
+  import commonEchart from '@/components/commonEchart.vue'
+  const { pos } = defineProps({
+    pos: {
+      type: String,
+      required: true
+    }
+  })
+
+  // 定义 data
+  let data = reactive({
+    isSet: null, // 为了做判断：当鼠标移动上去的时候，自动高亮就被取消
+    charPie3currentIndex: 0,
+    myColor: ['#2975d4', '#41c7ff', '#30efbf'],
+    dataList1: [4049, 4950, 8267],
+    dataTitle: ['行为', '公益', '消费'],
+    dataSeries: [],
+    resObj: {},
+  })
+  data.dataList1.forEach((item, index) => {
+    data.dataSeries.push({
+      value: item,
+      name: data.dataTitle[index],
+    })
+  })
+  let total = computed(() => eval(data.dataList1.join("+")))
+  for (let i in data.dataSeries) {
+    data.resObj[data.dataSeries[i]["name"]] = data.dataSeries[i];
+  }
+
+  // 定义图表编辑项 option
+  let option = reactive({})
+  option = {
+    // backgroundColor: 'rgba(13,29,70,0.75)',
+    grid: {
+      left: '5%',
+      right: '8%',
+      bottom: '3%',
+      containLabel: true
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: "{b} : {c} ({d}%)"
+    },
+    legend: {
+      icon: "circle",
+      orient: 'vertical', // 布局方式，默认为水平布局，可选为：'horizontal' ¦ 'vertical'
+      right: '5%',
+      top:'23%',
+      textStyle: {
+        color: "#FFFFFF",
+        fontSize: 10,
+        rich:{
+          aaa:{
+            color:'#5fc8f3'
+          }
+        }
+      },
+      formatter: function(name) {
+        return name + '{aaa| ------ }' + data.resObj[name].value
+      }
+    },
+    color: data.myColor,
+    series: {
+      hoverOffset: 4,
+      itemGap: 20,
+      name: 'Nightingale Chart',
+      type: 'pie',
+      radius: [20, 50],
+      roseType: 'area',
+      center: ['27%', '50%'],
+      data: data.dataSeries,
+      label: {
+        formatter: '{b}\n',
+        padding: [0, -25, 40, -25],
+        color: "#ffffff",
+        fontSize:10,
+      },
+      labelLine: {
+        length: 5,
+        length2: 40,
+        lineStyle: {
+          color: "#ffffff"
+
+        }
+      },
+      // emphasis: {
+      //   scale: true,
+      //   scaleSize: 20,
+      //   focus: 'self',
+      //   label: {
+      //     // formatter: "{b}\n{d}%",
+      //     formatter: function(params){
+      //       return params.name+'\n'+'{tlabel|'+(params.value/total.value*100).toFixed(0)+'%'+'}'
+      //     },
+      //     show: true,
+      //     color: "#fff",
+      //     fontSize: 10,
+      //     lineHeight:24,
+      //     rich:{
+      //       tlabel:{
+      //         fontSize: 16,
+      //         // fontFamily:'DIN'
+      //       }
+      //     },
+      //     fontWeight: 'bold'
+      //   },
+      // },
+      itemStyle: {
+        borderRadius: 2,
+        // borderWidth: 10, //设置border的宽度有多大
+      },
+    }
+  }
+
+  // 处理进一步交互
+  let getMychart = (myChart) => {
+    console.log('ok')
+  }
+
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/neighborhood/index.vue
+++ src/views/neighborhood/index.vue
@@ -2,19 +2,34 @@
 	<bg>
 		<!-- 给bg组件传对应位置上的图表 -->
 		<template #first>
-			<chart-bg name="今日在园人数">
+			<chart-bg name="积分总数">
 				<!-- 给chart-bg组件传具体的图表 -->
 				<template #main>
-
+					<first-left pos="neighborhood-first-left"></first-left>
 				</template>
 			</chart-bg>
 		</template>
-
 		<template #second>
-			<chart-bg name="停车场情况">
+			<chart-bg name="积分来源">
 				<!-- 给chart-bg组件传具体的图表 -->
 				<template #main>
-
+					<second-left pos="neighborhood-second-left"></second-left>
+				</template>
+			</chart-bg>
+		</template>
+		<template #third>
+			<chart-bg name="积分趋势">
+				<!-- 给chart-bg组件传具体的图表 -->
+				<template #main>
+					<third-left pos="neighborhood-third-left"></third-left>
+				</template>
+			</chart-bg>
+		</template>
+		<template #forth>
+			<chart-bg name="用户积分分析">
+				<!-- 给chart-bg组件传具体的图表 -->
+				<template #main>
+					<forth-left pos="neighborhood-forth-left"></forth-left>
 				</template>
 			</chart-bg>
 		</template>
@@ -29,19 +44,40 @@
 			</chart-bg>
 		</template>
 		<template #second2>
-			<chart-bg name="工时实时统计">
+			<chart-bg name="工单实时统计">
+				<!-- 给chart-bg组件传具体的图表 -->
+				<template #main>
+					<second-right pos="neighborhood-second-right"></second-right>
+				</template>
+			</chart-bg>
+		</template>
+		<template #third2>
+			<chart-bg name="AI事件统计">
+				<!-- 给chart-bg组件传具体的图表 -->
+				<template #main>
+					<third-right pos="neighborhood-third-right"></third-right>
+				</template>
+			</chart-bg>
+		</template>
+		<template #forth2>
+			<chart-bg name="品质管理统计（年）">
 				<!-- 给chart-bg组件传具体的图表 -->
 				<template #main>
 
 				</template>
 			</chart-bg>
 		</template>
-
 	</bg>
 </template>
 <script setup>
 import bg from '@/components/bg.vue'
 import chartBg from '@/components/chartBg.vue'
+// import firstLeft from './echarts/firstLeft.vue'
+import secondLeft from './echarts/secondLeft.vue'
+import thirdLeft from './echarts/thirdLeft.vue'
+// import secondRight from './echarts/secondRight.vue'
+// import thirdRight from './echarts/thirdRight.vue'
+
 // let t = '积分总数'
 </script>
 
