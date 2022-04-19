6a7857116726e0e80916574952d0c3f453a62bfa
beyondllc <1534579826@qq.com>
2022-04-19 14:23:30 +0800

    底部按钮

--- src/components/homeFooter.vue
+++ src/components/homeFooter.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="homeFooter">
-    <div>
-
+    <div class="homeFooter-item " v-for="item in data.footerList" @click="toPush(item)">
+        <div class="item-title " :class="activeName===item.name?'active':''">{{item.name}}</div>
+        <div class="item-sub">{{item.ename}}</div>
     </div>
   </div>
 </template>
 <script setup>
-  import { reactive } from 'vue'
+  import { reactive,ref } from 'vue'
   let data=reactive({
     footerList:[
-      {id : 0,name : '总览'},
-      {id : 1,name : '邻里'},
-      {id : 2,name : '教育'},
-      {id : 3,name : '健康'},
-      {id : 4,name : '创业'},
-      {id : 5,name : '建筑'},
-      {id : 6,name : '建筑'},
-      {id : 7,name : '建筑'}
+      {id : 0,name : '总览',ename : 'overview'},
+      {id : 1,name : '邻里',ename : 'neighborhood'},
+      {id : 2,name : '教育',ename : 'education'},
+      {id : 3,name : '健康',ename : 'health'},
+      {id : 4,name : '创业',ename : 'entrepreneurship'},
+      {id : 5,name : '建筑',ename : 'building'},
+      {id : 6,name : '交通',ename : 'traffic'},
+      {id : 7,name : '低碳',ename : 'Low carbon'},
+      {id : 8,name : '服务',ename : 'service'},
+      {id : 9,name : '治理',ename : 'Governance'},
     ]
   })
+  let activeName=ref('总览')
+  function toPush(item){
+    
+    activeName.value=item.name
+    console.log(item.name)
+  }
 </script>
 <style lang="less" scoped>
 .homeFooter{
@@ -28,7 +37,30 @@
   left: 25%;
   height: 40px;
   width: 50%;
-  background: red;
+  background: linear-gradient(86deg, rgba(8, 19, 32, 0) 0%, rgba(8, 19, 32, 0.7) 52%, rgba(8, 19, 32, 0) 100%);
   display: flex;
+  justify-content: space-between;
+  .homeFooter-item{
+    width: 10%;
+    height: 40px;
+    text-align: center;
+    cursor: pointer;
+    .item-title{
+      height: 25px;
+      font-size: 1.0vw;
+      font-family: PingFang TC;
+      font-weight: 500;
+      line-height: 25px;
+      color: #FFFFFF;
+      background: linear-gradient(86deg, rgba(8, 19, 32, 0) 0%, rgba(8, 19, 32, 0.7) 52%, rgba(8, 19, 32, 0) 100%);
+      &.active{
+        background: linear-gradient(137deg, rgba(5, 57, 115, 0) 0%, #053973 100%);
+      }
+    }
+    .item-sub{
+      font-size: 0.6vw;
+      margin-top: 0px;
+    }
+  }
 }
 </style>
